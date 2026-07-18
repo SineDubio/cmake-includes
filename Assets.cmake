10fcec247f5322e9f0c5457d367885e95b4839fa
Sudara <sudara@alonetone.com>
2026-07-18 14:49:06 +0200

    Wrap helpers in pamplejuce_* functions for multi-product projects
    Legacy single-product consumers are unaffected: without PAMPLEJUCE_MULTI_PRODUCT set, each file still runs its include-time behavior.

--- Assets.cmake
+++ Assets.cmake
@@ -1,11 +1,29 @@
 # HEADS UP: Pamplejuce assumes anything you stick in the assets folder you want to included in your binary!
 # This makes life easy, but will bloat your binary needlessly if you include unused files
-file(GLOB_RECURSE AssetFiles CONFIGURE_DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/assets/*")
-list (FILTER AssetFiles EXCLUDE REGEX "/\\.DS_Store$") # We don't want the .DS_Store on macOS though...
 
-# Setup our binary data as a target called Assets
-juce_add_binary_data(Assets SOURCES ${AssetFiles})
+function(pamplejuce_add_assets)
+    set(oneValueArgs TARGET DIRECTORY)
+    cmake_parse_arguments(PJ_ASSETS "" "${oneValueArgs}" "" ${ARGN})
 
-# Required for Linux happiness:
-# See https://forum.juce.com/t/loading-pytorch-model-using-binarydata/39997/2
-set_target_properties(Assets PROPERTIES POSITION_INDEPENDENT_CODE TRUE)
+    if (NOT PJ_ASSETS_TARGET)
+        set(PJ_ASSETS_TARGET Assets)
+    endif ()
+    if (NOT PJ_ASSETS_DIRECTORY)
+        set(PJ_ASSETS_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/assets")
+    endif ()
+
+    file(GLOB_RECURSE AssetFiles CONFIGURE_DEPENDS "${PJ_ASSETS_DIRECTORY}/*")
+    list (FILTER AssetFiles EXCLUDE REGEX "/\\.DS_Store$") # We don't want the .DS_Store on macOS though...
+
+    # Setup our binary data as a target called Assets
+    juce_add_binary_data(${PJ_ASSETS_TARGET} SOURCES ${AssetFiles})
+
+    # Required for Linux happiness:
+    # See https://forum.juce.com/t/loading-pytorch-model-using-binarydata/39997/2
+    set_target_properties(${PJ_ASSETS_TARGET} PROPERTIES POSITION_INDEPENDENT_CODE TRUE)
+endfunction()
+
+# Legacy single-product consumers get today's exact behavior at include time.
+if (NOT PAMPLEJUCE_MULTI_PRODUCT)
+    pamplejuce_add_assets()
+endif ()
--- Benchmarks.cmake
+++ Benchmarks.cmake
@@ -1,26 +1,54 @@
-file(GLOB_RECURSE BenchmarkFiles CONFIGURE_DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/benchmarks/Catch2Main.cpp" "${CMAKE_CURRENT_SOURCE_DIR}/benchmarks/*.h")
+# Relies on Tests.cmake having been included first (Catch2 package + catch_discover_tests).
 
-# Organize the test source in the Tests/ folder in the IDE
-source_group(TREE ${CMAKE_CURRENT_SOURCE_DIR}/benchmarks PREFIX "" FILES ${BenchmarkFiles})
+function(pamplejuce_add_benchmarks)
+    set(oneValueArgs TARGET DIRECTORY PLUGIN_TARGET SHARED_CODE SOURCE_INCLUDE_DIR)
+    cmake_parse_arguments(PJ_BENCH "" "${oneValueArgs}" "" ${ARGN})
 
-add_executable(Benchmarks ${BenchmarkFiles})
-target_compile_features(Benchmarks PRIVATE cxx_std_20)
-catch_discover_tests(Benchmarks)
+    if (NOT PJ_BENCH_TARGET)
+        set(PJ_BENCH_TARGET Benchmarks)
+    endif ()
+    if (NOT PJ_BENCH_DIRECTORY)
+        set(PJ_BENCH_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/benchmarks")
+    endif ()
+    if (NOT PJ_BENCH_PLUGIN_TARGET)
+        set(PJ_BENCH_PLUGIN_TARGET "${PROJECT_NAME}")
+    endif ()
+    if (NOT PJ_BENCH_SHARED_CODE)
+        set(PJ_BENCH_SHARED_CODE SharedCode)
+    endif ()
+    if (NOT PJ_BENCH_SOURCE_INCLUDE_DIR)
+        set(PJ_BENCH_SOURCE_INCLUDE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/source")
+    endif ()
 
-# Our benchmark executable also wants to know about our plugin code...
-target_include_directories(Benchmarks PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/source)
+    file(GLOB_RECURSE BenchmarkFiles CONFIGURE_DEPENDS "${PJ_BENCH_DIRECTORY}/Catch2Main.cpp" "${PJ_BENCH_DIRECTORY}/*.h")
 
-# Copy over compile definitions from our plugin target so it has all the JUCEy goodness
-target_compile_definitions(Benchmarks PRIVATE $<TARGET_PROPERTY:${PROJECT_NAME},COMPILE_DEFINITIONS>)
+    # Organize the test source in the Tests/ folder in the IDE
+    source_group(TREE ${PJ_BENCH_DIRECTORY} PREFIX "" FILES ${BenchmarkFiles})
 
-# And give tests access to our shared code
-target_link_libraries(Benchmarks PRIVATE SharedCode Catch2::Catch2)
+    add_executable(${PJ_BENCH_TARGET} ${BenchmarkFiles})
+    target_compile_features(${PJ_BENCH_TARGET} PRIVATE cxx_std_20)
+    catch_discover_tests(${PJ_BENCH_TARGET})
 
-# Make an Xcode Scheme for the test executable so we can run tests in the IDE
-set_target_properties(Benchmarks PROPERTIES XCODE_GENERATE_SCHEME ON)
+    # Our benchmark executable also wants to know about our plugin code...
+    target_include_directories(${PJ_BENCH_TARGET} PRIVATE ${PJ_BENCH_SOURCE_INCLUDE_DIR})
 
-# When running Tests we have specific needs
-target_compile_definitions(Benchmarks PUBLIC
-    JUCE_MODAL_LOOPS_PERMITTED=1 # let us run Message Manager in tests
-    RUN_PAMPLEJUCE_TESTS=1 # also run tests in module .cpp files guarded by RUN_PAMPLEJUCE_TESTS
-)
+    # Copy over compile definitions from our plugin target so it has all the JUCEy goodness
+    target_compile_definitions(${PJ_BENCH_TARGET} PRIVATE $<TARGET_PROPERTY:${PJ_BENCH_PLUGIN_TARGET},COMPILE_DEFINITIONS>)
+
+    # And give tests access to our shared code
+    target_link_libraries(${PJ_BENCH_TARGET} PRIVATE ${PJ_BENCH_SHARED_CODE} Catch2::Catch2)
+
+    # Make an Xcode Scheme for the test executable so we can run tests in the IDE
+    set_target_properties(${PJ_BENCH_TARGET} PROPERTIES XCODE_GENERATE_SCHEME ON)
+
+    # When running Tests we have specific needs
+    target_compile_definitions(${PJ_BENCH_TARGET} PUBLIC
+        JUCE_MODAL_LOOPS_PERMITTED=1 # let us run Message Manager in tests
+        RUN_PAMPLEJUCE_TESTS=1 # also run tests in module .cpp files guarded by RUN_PAMPLEJUCE_TESTS
+    )
+endfunction()
+
+# Legacy single-product consumers get today's exact behavior at include time.
+if (NOT PAMPLEJUCE_MULTI_PRODUCT)
+    pamplejuce_add_benchmarks()
+endif ()
